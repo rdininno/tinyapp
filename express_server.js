49af86d9c5cf233b3689bb83a3ca8da9c3782a65
Robert Di Ninno <robertdininno@gmail.com>
2022-05-17 15:16:01 +0000

    add delete functionality

--- express_server.js
+++ express_server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const morgan = require('morgan'); 
+const morgan = require('morgan');
 const bodyParser = require("body-parser");
 const PORT = 8080; // default port 8080
 
 
 app.set("view engine", "ejs");
 app.use(morgan('dev')); //morgan
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,7 +17,7 @@ const urlDatabase = {
 function generateRandomString() {
   let results = '';
   const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  for(let ii = 0; ii < 6; ii++){
+  for (let ii = 0; ii < 6; ii++) {
     results += char.charAt(Math.floor(Math.random() * char.length));
   }
 
@@ -39,11 +39,11 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-    const templateVars = {
-        shortURL: req.params.shortURL,  
-        longURL: urlDatabase[req.params.shortURL].longURL
-    }
-    res.render("urls_show", templateVars);
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL].longURL
+  }
+  res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -52,14 +52,18 @@ app.get("/u/:shortURL", (req, res) => {
   return res.redirect(longURL);
 });
 
-
-
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
- 
-  res.redirect(`/urls/${shortURL}`);  
+
+  res.redirect(`/urls/${shortURL}`);
 })
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+
+  res.redirect("/urls");
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
